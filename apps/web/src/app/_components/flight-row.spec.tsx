a44f4111dd9c7e8963192572c34ab039e1f39ac0
remcomulders <remco_mulders@hotmail.com>
2024-11-04 17:28:17 +0100

    chore: code cleanup

--- apps/web/src/app/_components/flight-row.spec.tsx
+++ apps/web/src/app/_components/flight-row.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { FlightRow } from './flight-row';
 import '@testing-library/jest-dom';
 import { appSettings } from '../../constants/app-settings';
